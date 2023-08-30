0a568f49af36bc0c12b2d9875e396815120523ce
Mahbub-diu <mahbub.mediasoft@gmail.com>
2023-08-31 00:30:49 +0600

    update new dashboard

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,22 +1,26 @@
-"use client";
+'use client';
 
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export default function Navbar() {
-  let pathName=usePathname()
-  if(pathName=="/" || pathName=="/login" || pathName=="/signup"|| pathName.startsWith('/admin')
-  ){
-    return <></>
+  let pathName = usePathname();
+  if (
+    pathName == '/' ||
+    pathName == '/login' ||
+    pathName == '/signup' ||
+    pathName.startsWith('/admin')
+  ) {
+    return <></>;
   }
   let [todayDate, setTodayDate] = useState({
-    month: "Janu",
-    date: 1
+    month: 'Janu',
+    date: 1,
   });
   useEffect(() => {
     let date = new Date();
-    let month = date.toLocaleString("default", { month: "long" });
+    let month = date.toLocaleString('default', { month: 'long' });
     let dateNum = date.getDate();
     setTodayDate({
       month: month,
@@ -24,50 +28,90 @@ export default function Navbar() {
     });
   }, []);
   return (
-    <section className="fixed-nav-main">
-      <div className="container-xxl">
-        <div className="row">
-          <div className="col-12">
-            <div className="fixed-nav">
-              <ul className="nav justify-content-center">
-                <li className="nav-item">
-                  <Link className="nav-link active" href="/dashboard">
-                    <div className="calender-box">
-                      <div className="month-name">{todayDate.month}</div>
-                      <div className="date">{todayDate.date}</div>
-                    </div>
-                    dashboard
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" href="/story">
-                    <div className="icon-box">
-                      <img src="assets/images/icon/story.png" alt="" />
-                    </div>
-                    <div className="nav-title">story</div>
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" href="/sprints">
-                    <div className="icon-box">
-                      <img src="assets/images/icon/terminal.png" alt="" />
-                    </div>
-                    <div className="nav-title">Journey</div>
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" href="/reward">
-                    <div className="icon-box">
-                      <img src="assets/images/icon/star.png" alt="" />
-                    </div>
-                    <div className="nav-title">rewards</div>
-                  </Link>
-                </li>
-              </ul>
-            </div>
+    <section className="navbar-main">
+      <nav className="navbar navbar-expand-lg">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            <img src="assets/images/logo.png" alt="icon" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav justify-content-center m-auto">
+              <li className="nav-item">
+                <a className="nav-link " href="#">
+                  dashboard
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  quest
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  story
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  journey
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  leaderboard
+                </a>
+              </li>
+            </ul>
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link mastermind-link" href="#">
+                  enter mastermind
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <img src="assets/images/icon/user-icon.png" alt="icon" />
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      my profile
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      setting
+                    </a>
+                  </li>
+
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      log out
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
           </div>
         </div>
-      </div>
+      </nav>
     </section>
   );
 }
