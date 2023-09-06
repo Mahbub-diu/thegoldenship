b1bb1bfc9fed43bdecc657b9fe30c6b7a48d469d
Prantho-das <pranthokumardas786@gmail.com>
2023-09-06 15:52:54 +0600

    update

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -3,9 +3,8 @@ set -e
 
 echo "Deploying started..."
 git pull origin master
-git pull origin master
 echo "Installing dependencies..."
-npm install --force
+npm install --yes --force
 npm run build
 pm2 stop all
 pm2 start npm --name "thegoldenship" -- run start -- --port=8077
